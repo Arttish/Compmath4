ecaf839b94f566425dcb9798c14162e185d5b171
Arttish <super.art-tish@yandex.ru>
2024-11-29 01:59:00 +0300

    noize -> noise

--- main.cpp
+++ main.cpp
@@ -3,25 +3,26 @@
 #include "functions/func1.h"
 #include "pregresion/Polynomial_regression.h"
 #include "functions/Polynomial.h"
+#include "functions/func2.h"
 
 int main(int argc, char** argv) {
     int i = 1;
-    double start, end, step, noize;
+    double start, end, step, noise;
 
     start = std::stod(argv[i++]);
     end = std::stod(argv[i++]);
     step = std::stod(argv[i++]);
-    noize = std::stod(argv[i++]);
+    noise = std::stod(argv[i++]);
 
     std::ofstream s("../data.txt");
     std::ofstream answer("../ans.txt");
     std::ofstream res("../res.txt");
 
-    Generator gen(noize, &s);
+    Generator gen(noise, &s);
     Generator ans_gen(0, &answer);
     Generator res_gen(0, &res);
 
-    IFunc* f = new func1;
+    IFunc* f = new func2;
 
     auto data = gen.generate(f, start, end, step);
     ans_gen.generate(f, start, end, step);
